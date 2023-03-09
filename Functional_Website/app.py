4161a5fb2dc79a05209ccf32deb573ba64dae687
Chalermchai Kamlungdach <chalermchaikm@gmail.com>
2023-03-09 22:40:12 +0700

    First_Ver_Full_Functional_WEBSITE

---
+++ Functional_Website/app.py
@@ -0,0 +1,40 @@
+import os
+
+from flask import Flask, request, render_template, redirect, url_for, send_from_directory
+from werkzeug.utils import secure_filename
+from datetime import datetime
+from dst_script import dst_process
+
+ALLOWED_EXTENSIONS = set(['csv'])
+
+app = Flask(__name__)
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        file = request.files['file']
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            new_filename = f'{filename.split(".")[0]}.csv'
+            save_location = os.path.join('input', new_filename)
+            file.save(save_location)
+
+            output_file = dst_process(save_location)
+            return redirect(url_for('download'))
+
+    return render_template('upload.html')
+
+@app.route('/download')
+def download():
+    return render_template('download.html', files=os.listdir('output'))
+
+@app.route('/download/<filename>')
+def download_file(filename):
+    return send_from_directory('output', filename)
+
+if __name__ == "__main__":
+    app.run(host='localhost', port=5000, debug=True)
---
+++ Functional_Website/dst_script.py
@@ -0,0 +1,23 @@
+import csv
+import os
+
+from collections import defaultdict
+from datetime import datetime
+from decimal import Decimal
+import pandas as pd
+
+def dst_process(filename):
+    name = filename.split('\\')
+    name = name[len(name)-1].split('.')
+    name = name[0]
+    data = {'Name': ['MR.BEANS','MR.BEANS','MR.BEANS'], 'Status': ['LOL','LOL','LOL']}
+    df_hotel_output = pd.DataFrame(data)
+    output_file = f'result_{name}.csv'
+    with open(os.path.join('output', output_file), 'w', newline='',encoding='utf-8') as f:
+        writer = csv.writer(f)
+        writer.writerow(['Name', 'Status'])
+
+        for i in range(len(df_hotel_output)):
+            writer.writerow([df_hotel_output["Name"][i], df_hotel_output["Status"][i]])
+
+    return output_file
