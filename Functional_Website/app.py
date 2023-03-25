3f72d30910aa000f8074da05dbc370c630133c24
Chalermchai Kamlungdach <chalermchaikm@gmail.com>
2023-03-25 14:40:10 +0700

    functional website_03

--- Functional_Website/app.py
+++ Functional_Website/app.py
@@ -1,9 +1,10 @@
 import os
 
+import pandas as pd
 from flask import Flask, request, render_template, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 from datetime import datetime
-from dst_script import dst_process
+from dst_script import dst_process,input_validation
 
 ALLOWED_EXTENSIONS = set(['csv'])
 
@@ -15,6 +16,8 @@ def allowed_file(filename):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    invalid = False
+    table = pd.read_csv("example/variable.csv", encoding = "ISO-8859-1")
     if request.method == 'POST':
         file = request.files['file']
         if file and allowed_file(file.filename):
@@ -23,14 +26,27 @@ def index():
             save_location = os.path.join('input', new_filename)
             file.save(save_location)
 
-            output_file,df = dst_process(save_location)
-            return render_template('download.html', files=os.listdir('output'), table = df.to_html())
+            if not input_validation(save_location):
+                invalid = True
+            else:
+                df = dst_process(save_location)
+                return render_template('download.html', files=os.listdir('output'), table = df.to_html())
+        else:
+            invalid = True
 
-    return render_template('upload.html')
+    elif request.method == 'GET':
+        invalid = False
+    
+    return render_template('upload.html', invalid=invalid , files=os.listdir('example') , table = table.to_html())
 
 @app.route('/download/<filename>')
 def download_file(filename):
     return send_from_directory('output', filename)
+
+@app.route('/download_example/<filename>')
+def download_example_file(filename):
+    return send_from_directory('example', filename)
+
 
 if __name__ == "__main__":
     app.run(host='localhost', port=5000, debug=True)
--- Functional_Website/dst_script.py
+++ Functional_Website/dst_script.py
@@ -5,24 +5,192 @@ from collections import defaultdict
 from datetime import datetime
 from decimal import Decimal
 import pandas as pd
+import numpy as np
+import missingno as msno
+
+import warnings
+warnings.filterwarnings('ignore')
+
+from sklearn.tree import DecisionTreeClassifier
+
+
+def input_validation(filename):
+    df = pd.read_csv(filename)
+    df_val = pd.read_csv(f"validation/input_val.csv")
+
+    if df.columns.equals(df_val.columns):
+        return True
+
+    return False
+
+
+###ใช้แก้ข้อมูลพวก NULL ใน Dataframe
+def FillNA(df):
+    # แยกค่า NULL
+    null = pd.DataFrame({'Null Values' : df.isna().sum(), 'Percentage Null Values' : (df.isna().sum()) / (df.shape[0]) * (100)})
+
+    # filling null values with zero
+    df.fillna(0, inplace = True)
+
+    # ทั้ง 3 collumn จะเป็น 0 หมด  ไม่ได้
+    filter = (df.children == 0) & (df.adults == 0) & (df.babies == 0)
+
+    # เอา Record ที่ 3 column เป็น 0 ออก
+    df = df[~filter]
+    
+    return df
+
+
+###ใช้กรอก Column ให้เหลือเฉพาะที่ต้องการๅ
+def FilterColumn(df):
+    
+    FeatureList = [
+    'hotel',
+    'lead_time',
+    'arrival_date_month',
+    'arrival_date_week_number',
+    'arrival_date_day_of_month',
+    'stays_in_weekend_nights',
+    'stays_in_week_nights',
+    'adults',
+    'children',
+    'babies',
+    'meal',
+    'market_segment',
+    'distribution_channel',
+    'is_repeated_guest',
+    'previous_cancellations',
+    'previous_bookings_not_canceled',
+    'reserved_room_type',
+    'deposit_type',
+    'agent',
+    'company',
+    'customer_type',
+    'adr',
+    'required_car_parking_spaces',
+    'total_of_special_requests',
+    'reservation_status_date',
+    'name'
+    
+    ,'Test1'
+    ,'Test2'
+    ,'Test3'
+    ,'Test4'
+    ,'Test5'
+    
+    ]
+
+    ###   https://www.sciencedirect.com/science/article/pii/S2352340918315191
+    
+    
+    clist = []
+    for c in df:
+        if(c not in FeatureList):
+            df = df.drop(columns=c)
+    
+    
+    for c in df:
+        clist.append(c)
+    
+    
+    clist=clist.sort()
+    FeatureList=FeatureList.sort()
+    if(clist != FeatureList):
+        print("Error : Data doesn't met requiment")
+        for c in clist:
+            if c not in FeatureList:
+                print(c+' not found!')
+        return 0
+    
+    return df
+
+
+####แปลงข้อมูลใน Dataframe  ทั้งหมด
+#-FillNA
+#-FileterColumn
+#-แปลง Categorical เป็น Label
+#-Nomalize ค่าใน Dataframe
+#แยก column ชื่่อคนออกมา
+
+#Return DF-X และ DFชื่อคน
+def PreProcessingData(df):
+    
+    dfname = df["name"]
+    df.drop(['name'] , axis = 1, inplace = True)
+    
+    df = FilterColumn(df)
+    if (not isinstance(df, pd.DataFrame)):
+        return 0
+    df = FillNA(df)
+    
+    
+    # creating categorical dataframes
+    cat_cols = [col for col in df.columns if df[col].dtype == 'O']
+    cat_df = df[cat_cols]
+
+    cat_df['reservation_status_date'] = pd.to_datetime(cat_df['reservation_status_date'])
+    cat_df['year'] = cat_df['reservation_status_date'].dt.year
+    cat_df['month'] = cat_df['reservation_status_date'].dt.month
+    cat_df['day'] = cat_df['reservation_status_date'].dt.day
+    cat_df.drop(['reservation_status_date','arrival_date_month'] , axis = 1, inplace = True)
+    cat_df['hotel'] = cat_df['hotel'].map({'Resort Hotel' : 0, 'City Hotel' : 1})
+    cat_df['meal'] = cat_df['meal'].map({'BB' : 0, 'FB': 1, 'HB': 2, 'SC': 3, 'Undefined': 4})
+    cat_df['market_segment'] = cat_df['market_segment'].map({'Direct': 0, 'Corporate': 1, 'Online TA': 2, 'Offline TA/TO': 3,
+                                                             'Complementary': 4, 'Groups': 5, 'Undefined': 6, 'Aviation': 7})
+    cat_df['distribution_channel'] = cat_df['distribution_channel'].map({'Direct': 0, 'Corporate': 1, 'TA/TO': 2, 'Undefined': 3,
+                                                                           'GDS': 4})
+    cat_df['reserved_room_type'] = cat_df['reserved_room_type'].map({'C': 0, 'A': 1, 'D': 2, 'E': 3, 'G': 4, 'F': 5, 'H': 6,
+                                                                       'L': 7, 'B': 8})
+    cat_df['deposit_type'] = cat_df['deposit_type'].map({'No Deposit': 0, 'Refundable': 1, 'Non Refund': 3})
+    cat_df['customer_type'] = cat_df['customer_type'].map({'Transient': 0, 'Contract': 1, 'Transient-Party': 2, 'Group': 3})
+    cat_df['year'] = cat_df['year'].map({2015: 0, 2014: 1, 2016: 2, 2017: 3})
+    
+    
+    
+    
+    num_df = df.drop(columns = cat_cols, axis = 1)
+    num_df
+
+    # normalizing numerical variables
+
+    num_df['lead_time'] = np.log(num_df['lead_time'] + 1)
+    num_df['arrival_date_week_number'] = np.log(num_df['arrival_date_week_number'] + 1)
+    num_df['arrival_date_day_of_month'] = np.log(num_df['arrival_date_day_of_month'] + 1)
+    num_df['agent'] = np.log(num_df['agent'] + 1)
+    num_df['company'] = np.log(num_df['company'] + 1)
+    num_df['adr'] = np.log(num_df['adr'] + 1)
+    num_df['adr'] = num_df['adr'].fillna(value = num_df['adr'].mean())
+    
+    
+    X = pd.concat([cat_df, num_df], axis = 1)
+
+    return X, dfname
+
+
+def loadModel(path):
+    from joblib import load
+    dtc = load(path)
+    return dtc
+
 
 def dst_process(filename):
     name = filename.split('\\')
     name = name[len(name)-1].split('.')
     name = name[0]
-    data = {'Name': ['MR.BEANS','MR.BEANS','MR.BEANS'], 'Status': ['LOL','LOL','LOL']}
-    # df_hotel_output = pd.read_csv(filename)
-    df_hotel_output = pd.DataFrame(data)
-
-    # Processing 
+    df, dfName = PreProcessingData(pd.read_csv(filename))
+    dfName.name = "ชื่อ"
     
+    dtc = loadModel(r'./dtc.joblib')  #โหลด Model Decision Tree
+    pred = dtc.predict(df)   #Predict ว่ามีโอกาส Cancel   (Return Array)
+    pred = pd.DataFrame(pred, columns = ['สถานะการจอง'])  #แปลง Array เป็น df
+    df = pd.concat([dfName, pred], axis = 1) #เอาผล pred มาต่อกับ name
 
-    output_file = f'result_{name}.csv'
-    with open(os.path.join('output', output_file), 'w', newline='',encoding='utf-8') as f:
-        writer = csv.writer(f)
-        writer.writerow(['Name', 'Status'])
+    df['สถานะการจอง'] = df['สถานะการจอง'].apply(lambda x: 'ไม่ยกเลิก' if x == 1 else 'ยกเลิก')
 
-        for i in range(len(df_hotel_output)):
-            writer.writerow([df_hotel_output["Name"][i], df_hotel_output["Status"][i]])
+    # df = df[df['is_possible_to_cancel']=="Cancel"]
+    
+    # output_file = f'result_{name}.csv'
 
-    return output_file,df_hotel_output
+    df.to_csv(f"output/result_{name}.csv")
+
+    return df
